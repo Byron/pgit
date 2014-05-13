432a3541e4041dcd583ea397b01a87e898a95d6f
Sebastian Thiel <byronimo@gmail.com>
2014-05-13 08:55:49 +0200

    It's better to use an assertion error as marker, I suppose

--- src/python/pgit/cmd/submodule.py
+++ src/python/pgit/cmd/submodule.py
@@ -201,9 +201,9 @@ repository if it contains any user modifications"
 
             if cmd not in lut:
                 # raise a more special exception, sometimes git-python raises KeyError as well
-                raise EnvironmentError
+                raise AssertionError
             lut[cmd](args)
-        except EnvironmentError:
+        except AssertionError:
             raise InputError("Invalid operation: %r" % cmd)
         #END handle operation
         
